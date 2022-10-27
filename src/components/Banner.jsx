1e9be4ea42a4f00bb1fd8227b687cd04babe6848
Roch Armengaud <armengaud.roch@live.fr>
2022-10-27 15:51:16 +0200

    Small fixes

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,10 +4,12 @@ import '../styles/Banner.css';
 
 export default function Banner() {
   return (
-    <div className="home-banner py-10">
-      <img className="island rounded-3xl object-cover w-full h-full" src={Island} alt="Island landscape"></img>
+    <div className="home-banner px-6">
+      <img className="island rounded-xl brightness-90 h-28 object-cover object-right-top w-full" src={Island} alt="Island landscape"></img>
       <div className="text-over">
-        <h1>Chez vous, partout et ailleurs</h1>
+        <h1 className="text-2xl font-medium leading-7 text-white">
+          Chez vous,<br></br> partout et ailleurs
+        </h1>
       </div>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,20 @@
 import logo from '../assets/kasa logo.png';
 import '../styles/Global.css';
 import Banner from '../components/Banner';
+import Card from '../components/Card';
 function Homepage() {
   return (
     <>
-      <div className="global-padding px-40">
-        <nav className="navbar-container flex items-center justify-between w-full py-10">
-          <img className="kasa-logo w-52 h-16" src={logo} alt="Kasa logo"></img>
-          <div className="nav-items flex gap-40 pr-10">
-            <p className="hover:underline-offset-2 hover:underline">Accueil</p>
-            <p className="hover:underline-offset-2 hover:underline">A propos</p>
-          </div>
-        </nav>
-        <Banner />
+      <nav className="navbar-container flex items-center justify-between w-full px-6 py-6">
+        <img className="kasa-logo w-36 h-12" src={logo} alt="Kasa logo"></img>
+        <div className="nav-items flex gap-4">
+          <p className="hover:underline-offset-2 hover:underline">Accueil</p>
+          <p className="hover:underline-offset-2 hover:underline">A propos</p>
+        </div>
+      </nav>
+      <Banner />
+      <div className="cards-container">
+        <Card />
       </div>
     </>
   );
