ee07dc8ec2b9dffcf7eb6ff1ff46d11c091dc06d
Roch Armengaud <armengaud.roch@live.fr>
2022-11-01 15:22:26 +0100

    Set up the id route in the URL

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,26 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import '../styles/Card.css';
 
 export default function Card(props) {
-  const pictureId = props.data.filter((element) => {
-    return element.id;
-  });
-
-  console.log(pictureId);
-
   return (
     <>
       {/* ici on écris props.data car on à choisi d'écrire le mot data dans home.jsx ligne 18 */}
       {props.data.map((value, index) => (
         <div className="card mb-6 overflow-hidden border-2 border-gray-200 rounded-lg" key={value.id}>
-          <div className="card-image h-50 w-full">
-            <img src={value.cover} alt={value.title} />
-          </div>
-          <div className="card-info lg:bg-white p-4">
-            <div className="card-title pb-2 leading-5">{value.title}</div>
-            <div className="card-location font-semibold">{value.location}</div>
-          </div>
+          <Link to={'/FicheLogement/' + value.id}>
+            <div className="card-image h-50 w-full">
+              <img src={value.cover} alt={value.title} />
+            </div>
+            <div className="card-info lg:bg-white p-4">
+              <div className="card-title pb-2 leading-5">{value.title}</div>
+              <div className="card-location font-semibold">{value.location}</div>
+            </div>
+          </Link>
         </div>
       ))}
     </>
