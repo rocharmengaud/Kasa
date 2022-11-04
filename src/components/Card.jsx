fed7959b4bfce54cbe74e63f157d5784a24241de
Roch Armengaud <armengaud.roch@live.fr>
2022-11-04 15:03:47 +0100

    fix on responsive css cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,8 +8,8 @@ export default function Card(props) {
       {props.data.map((value, index) => (
         <div className="card relative mb-6 overflow-hidden border-2 border-gray-200 rounded-lg" key={value.id}>
           <Link to={'/FicheLogement/' + value.id}>
-            <div className="card-image h-50 w-full">
-              <img className="h-[13rem]" src={value.cover} alt={value.title} />
+            <div className="card-image h-50">
+              <img className="h-[13rem] w-full object-cover" src={value.cover} alt={value.title} />
             </div>
             <div className="card-title bottom-4 left-4 absolute font-bold text-white">{value.title}</div>
           </Link>
