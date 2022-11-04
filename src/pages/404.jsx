6de6b2b71dc3dd2e7c027d3b3c8ad63e43615a12
Roch Armengaud <armengaud.roch@live.fr>
2022-11-04 14:22:19 +0100

    404 page done

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -11,9 +11,9 @@ export default function Error404() {
     <>
       <div className="content lg:px-32 px-6">
         <Navbar />
-        <div className="big-text text-7xl pt-32 font-bold text-center">404</div>
-        <div className="big-text pt-4 text-xl font-bold text-center">Oups! La page que vous demandez n'existe pas.</div>
-        <div className="big-text hover:underline-offset-2 hover:underline pt-24 text-lg font-normal text-center">
+        <div className="big-text text-7xl pt-32 font-black text-center lg:text-[14rem]">404</div>
+        <div className="big-text pt-6 text-xl text-center">Oups! La page que vous demandez n'existe pas.</div>
+        <div className="big-text hover:underline-offset-2 hover:underline font-extralight lg:pt-36 pt-24 text-center">
           <Link to="/">Retourner sur la page d'accueil</Link>
         </div>
         <div className="footer">
