a4d08279adcd4dabc7b25576ba5d62a7f486bc1c
Roch Armengaud <armengaud.roch@live.fr>
2022-11-04 15:30:43 +0100

    imported some more data

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -19,7 +19,9 @@ export default function Rating({ data }) {
         </div>
         <div className="profile flex items-center gap-2">
           <div className="profile-name">{data.name}</div>
-          <div className="profile-picture bg-slate-200 rounded-full w-[2rem] h-[2rem]"></div>
+          <div className="profile-picture">
+            <img className="w-[5rem] h-[5rem] rounded-full" src={data.picture} alt={data.name} />
+          </div>
         </div>
       </div>
     </>
