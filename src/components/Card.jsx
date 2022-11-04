151a836325b543915fa29b2325ef8fa4cfc64fa8
Roch Armengaud <armengaud.roch@live.fr>
2022-11-04 14:14:21 +0100

    Homepage done

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import '../styles/Card.css';
-
 export default function Card(props) {
   return (
     <>
       {/* ici on écris props.data car on à choisi d'écrire le mot data dans home.jsx ligne 18 */}
       {props.data.map((value, index) => (
-        <div className="card mb-6 overflow-hidden border-2 border-gray-200 rounded-lg" key={value.id}>
+        <div className="card relative mb-6 overflow-hidden border-2 border-gray-200 rounded-lg" key={value.id}>
           <Link to={'/FicheLogement/' + value.id}>
             <div className="card-image h-50 w-full">
-              <img src={value.cover} alt={value.title} />
+              <img className="h-[13rem]" src={value.cover} alt={value.title} />
             </div>
-            <div className="card-info lg:bg-white p-4">
-              <div className="card-title pb-2 leading-5">{value.title}</div>
-              <div className="card-location font-semibold">{value.location}</div>
-            </div>
+            <div className="card-title bottom-4 left-4 absolute font-bold text-white">{value.title}</div>
           </Link>
         </div>
       ))}
