2e5d9a779cdadadca95eba941af608998ecd361d
Roch Armengaud <armengaud.roch@live.fr>
2022-11-09 17:29:49 +0100

    cleaning files

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -4,16 +4,14 @@ import { Link } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
-import '../styles/404.css';
-
 export default function Error404() {
   return (
     <>
       <div className="content lg:px-32 px-6">
         <Navbar />
-        <div className="big-text text-7xl pt-32 font-black text-center lg:text-[14rem]">404</div>
-        <div className="big-text pt-6 text-xl text-center">Oups! La page que vous demandez n'existe pas.</div>
-        <div className="big-text hover:underline-offset-2 hover:underline font-extralight lg:pt-36 pt-24 text-center">
+        <div className="text-[#ff6060] text-7xl pt-32 font-black text-center lg:text-[14rem]">404</div>
+        <div className="text-[#ff6060] pt-6 text-xl text-center">Oups! La page que vous demandez n'existe pas.</div>
+        <div className="text-[#ff6060] hover:underline-offset-2 hover:underline font-extralight lg:pt-36 pt-24 text-center">
           <Link to="/">Retourner sur la page d'accueil</Link>
         </div>
         <div className="footer absolute bottom-0 left-0 right-0">
