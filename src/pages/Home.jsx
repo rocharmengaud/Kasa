ac5a7a64aa126366c873850f5a36ee724db98562
Roch Armengaud <armengaud.roch@live.fr>
2022-11-14 15:04:44 +0100

    Modified a comment

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,7 @@ function Homepage() {
     <>
       <div className="content lg:px-32 px-6">
         <Navbar />
-        {/* on met ici des espace insécables pour forcer un br */}
+        {/* on met ici des espace insécables pour forcer un br en version mobile*/}
         <Banner imgSrc={Island} description={'Chez vous, partout et ailleurs'} />
         <div className="box-container lg:p-8 lg:bg-[#F6F6F6] lg:rounded-3xl lg:mb-6">
           <div className="cards-container lg:grid lg:grid-cols-3 lg:gap-8 flex flex-col">
